15fb713996c0f93a5fff5d9f6fbd0c2186e36924
VladislavKornyushin <kornyushin.vladislav@yandex.ru>
2023-08-17 22:05:52 +0300

    NewsPortal 17.08.2023

--- NewsPortal_django/settings.py
+++ NewsPortal_django/settings.py
@@ -200,7 +200,7 @@ EMAIL_HOST = 'smtp.yandex.ru'             # адрес сервера Яндек
 EMAIL_PORT = 465                          # порт smtp сервера тоже одинаковый
 EMAIL_HOST_USER = 'Kornyushin.Vladislav@yandex.ru'
 # EMAIL_HOST_USER = 'Kornyushin.Vladislav'  # ваше имя пользователя, например, если ваша почта user@yandex.ru, то сюда надо писать user, иными словами, это всё то что идёт до собаки
-EMAIL_HOST_PASSWORD = 'x5gZ4LenZP}.i/#a'  # пароль от почты
+EMAIL_HOST_PASSWORD = ''  # пароль от почты
 EMAIL_USE_SSL = True                      # Яндекс использует ssl, подробнее о том, что это, почитайте в дополнительных источниках, но включать его здесь обязательно
 
 ADMINS = [
